90cde4b831f44dd786c803cc8f58dfe25a4d2f3e
maiam <pedrosergiomaia@gmail.com>
2018-03-07 00:44:25 -0300

    Atualizando ContatoDAO

--- agenda/src/br/com/infnet/contatos/ContatoDAO.java
+++ agenda/src/br/com/infnet/contatos/ContatoDAO.java
@@ -12,16 +12,16 @@ import java.util.ArrayList;
 
 
 
-public class ContatoDAO {
-    
-    public List<Contato> listar() throws DAOException{
+public class ContatoDAO extends Contato {
+
+    public List<Contato> listar() throws DAOException {
         
         List<Contato> retorno = new ArrayList<>();
         Connection conexao = null;
         try {
             
             conexao = FabricaDeConexoes.conectar();
-            String sql = "SELECT * FROM contatos"; // A Query a ser executada.
+            String sql = "SELECT * FROM contatos";
             Statement stmt = conexao.createStatement(); //
             ResultSet rs = stmt.executeQuery(sql);
             
@@ -32,53 +32,87 @@ public class ContatoDAO {
                 contato.setNome(rs.getString("nome"));
                 contato.setEmail(rs.getString("email"));
                 contato.setFone(rs.getString("fone"));
-                //------------------------------------
                 retorno.add(contato);
-                
-            }
+            }//while
             
         } catch (Exception e) {
             
-            throw new DAOException();
-            
-        }finally {
+            throw new DAOException("Houve um problema ao conectar com o banco de dados.");
+        } finally {
             
             FabricaDeConexoes.desconectar(conexao);
-            
-        }
-        
+        }//trycatch finally
         
         return retorno;
-        
-    }
+    }//listar
     
-    public void inserir (Contato contato) throws DAOException{
-        
+    public void inserir(Contato contato) throws DAOException {
+
         Connection conexao = null;
         try {
-            
+
             conexao = FabricaDeConexoes.conectar();
+            conexao.setAutoCommit(false);
             String sql = "INSERT INTO contatos (nome, email, fone) VALUES (?,?,?)"; // A Query a ser executada.
             PreparedStatement ps = conexao.prepareStatement(sql);
-            ps.setString (1, contato.getNome());
-            ps.setString (2, contato.getEmail());
-            ps.setString (1, contato.getFone());
+            ps.setString(1, contato.getNome());
+            ps.setString(2, contato.getEmail());
+            ps.setString(3, contato.getFone());
             ps.executeUpdate();
+            conexao.commit();
             
-                        
         } catch (Exception e) {
-            
-            throw new DAOException();
-            
-        }finally {
+
+            throw new DAOException("Houve um problema ao conectar com o banco de dados.");
+
+        } finally {
             
             FabricaDeConexoes.desconectar(conexao);
+        }//trycatch finally
+    }//inserir
+    
+    public void alterar(Contato contato) throws DAOException {
+
+        Connection conexao = null;
+        try {
+
+            conexao = FabricaDeConexoes.conectar();
+            conexao.setAutoCommit(false);
+            String sql = "UPDATE contatos SET nome=?, email=?, fone=? WHERE id=?"; // A Query a ser executada.
+            PreparedStatement ps = conexao.prepareStatement(sql);
+            ps.setString(1, contato.getNome());
+            ps.setString(2, contato.getEmail());
+            ps.setString(3, contato.getFone());
+            ps.setString(4, String.valueOf(contato.getId()));
+            int updatecount = ps.executeUpdate();
+            conexao.commit();
+        } catch (Exception e) {
+
+            throw new DAOException("Houve um problema ao conectar com o banco de dados.");
+        } finally {
             
-        }
-        
-        
-        
-    }
+            FabricaDeConexoes.desconectar(conexao);
+        }//trycatch finally
+    }//alterar
     
-    
-}
+    public void excluir(Contato contato) throws DAOException {
+
+        Connection conexao = null;
+        try {
+
+            conexao = FabricaDeConexoes.conectar();
+            conexao.setAutoCommit(false);
+            String sql = "DELETE FROM contatos WHERE id=?"; // A Query a ser executada.
+            PreparedStatement ps = conexao.prepareStatement(sql);
+            ps.setString(1, String.valueOf(contato.getId()));
+            int deletecount = ps.executeUpdate();
+            conexao.commit();
+        } catch (Exception e) {
+
+            throw new DAOException("Houve um problema ao conectar com o banco de dados.");
+        } finally {
+            
+            FabricaDeConexoes.desconectar(conexao);
+        }//trycatch finally
+    }//excluir
+}//classDF
